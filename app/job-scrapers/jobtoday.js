48dfe1d53606915a260723bbeb4dd5736eab0e6f
Rodrigo Soto Cid <rodrigo.soto.web82@gmail.com>
2024-06-26 15:36:06 +0200

    waitForSelector

--- app/job-scrapers/jobtoday.js
+++ app/job-scrapers/jobtoday.js
@@ -18,6 +18,7 @@ const Jobtoday = async () => {
 
         // modal cookies
         if (pagina === 1) {
+            await page?.waitForSelector('div.modal-bottom.mt-8>div>button.btn.jt-btn-primary');
             await page?.click('div.modal-bottom.mt-8>div>button.btn.jt-btn-primary');
         }
 
